b87adb188504220b9273e16f3e5d61a4d4ee4f36
DzGransilV2 <gransildsouza2@gmail.com>
2024-03-15 21:30:44 +0530

    #updateByDz

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ function App() {
       {/* <Room> */}
       {/* <Drawing/> */}
       {/* <Home/> */}
-      {/* <Shop/> */}
+      <Shop/>
       {/* <Profile/> */}
       {/* </Room> */}
       {/* <Rhstudio/> */}
-      <Getstarted/>
+      {/* <Getstarted/> */}
     </>
   );
 }
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './shop.css'
 import Nav from '../home/home_componet/Navbar'
+import Upload from '../rhstudio/rhstudio_component/Upload'
+import ProfileRadiusPic from '../rhstudio/rhstudio_component/ProfileRadiusPic'
 
 const Shop = () => {
   return (
-    <div>
-      <Nav/>
+    <div className='shopPage'>
+      <div className='navContainer'>
+        <Nav/>
+      </div>
+      <div className='shopContainer'>
+        <div className="topNav"><Upload/><ProfileRadiusPic/></div>
+        <div className='shopCards'>
+          <div className='shopCard'>
+            <div className='shopImg'></div>
+            <div className="shopInfo">
+              <div className='shopInfoMain'>
+                <span>Title Here..</span>
+                <span>Description Here..</span>
+              </div>
+              <div className='shopInfoFooter'>
+                <span className='shopInfoUsername'>By:username</span>
+                <span className='shopInfoPrice'>$price</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
